3020227323be6d596d06e0d61be852b225239b00
Lucy <lucyng27@gmail.com>
2017-02-06 20:14:03 +0300

    Start the tests

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+from primenumbers import prime_numbers
+class Prime_tests(unittest.TestCase):
+
+	def test_prime_numbers_10(self):
+		self.assertEqual(prime_numbers(10), [2,3,5,7])
+
+	def test_prime_numbers_int(self):
+	    self.assertTrue(prime_numbers("b", "Try an integer "))
+
+if __name__ == '__main__':
+	unittest.main()
